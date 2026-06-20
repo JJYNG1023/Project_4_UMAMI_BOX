c0afa2869a6a8689c6f1e73e7f1f397c74f5b891
JJYNG1023 <j.yeung2020@outlook.com>
2026-06-20 15:05:47 +0100

    create shop.html and finalise layout for different screen views

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='shop'),
+    path('', views.shop, name='shop'),
     path('items/', views.shop_items, name='shop_items'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
