bc042b6cbff68748d49242ea5da927035a843ed0
JJYNG1023 <j.yeung2020@outlook.com>
2026-06-28 10:57:35 +0100

    update product detail link in index.html

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from shop.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    recommended_products = Product.objects.filter(is_available=True)[:5]
+
+    context = {
+        'recommended_products': recommended_products,
+    }
+
+    return render(request, 'home/index.html', context)
