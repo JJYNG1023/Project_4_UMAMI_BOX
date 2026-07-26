bcd1c743675095bbe4a259b4dd0ce5f79c584a05
JJYNG1023 <j.yeung2020@outlook.com>
2026-07-26 10:57:30 +0100

    create form.py in product and add draft product form

---
+++ shop/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Product 
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model:Product
+        fields:'__all__'
+
+    def __init__ (self , *args **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field.name, field in self.fields.items():
+            field.widget.attrs['class'] = 'profile-form-input'
