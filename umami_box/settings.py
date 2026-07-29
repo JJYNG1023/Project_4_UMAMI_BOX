44e66f6664a1944219987a83992dc186c48a03bc
JJYNG1023 <j.yeung2020@outlook.com>
2026-07-29 20:17:55 +0100

    add S3 bucket and AWS for deployment

--- umami_box/settings.py
+++ umami_box/settings.py
@@ -64,6 +64,9 @@ INSTALLED_APPS = [
     'basket',
     'profiles',
     'checkout',
+
+    #others
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -186,6 +189,24 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 #media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+if 'USE_AWS' in os.environ:
+    #Bucket config
+    AWS_STORAGE_BUCKET_NAME = 'umami-box-jason-2026'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY= os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    #static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION='static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    #override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Cloudinary media storage
 STORAGES = {
